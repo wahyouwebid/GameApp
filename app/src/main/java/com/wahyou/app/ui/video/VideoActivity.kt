b347599206152e50360f58923e7857a816190f0b
wahyouwebid <ujangwahyusst@gmail.com>
2021-05-31 19:16:19 +0700

    add video player

--- app/src/main/java/com/wahyou/app/ui/video/VideoActivity.kt
+++ app/src/main/java/com/wahyou/app/ui/video/VideoActivity.kt
@@ -1,46 +1,50 @@
 package com.wahyou.app.ui.video
 
-import android.annotation.SuppressLint
-import android.net.Uri
+import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.util.Log
-import android.webkit.WebChromeClient
-import android.webkit.WebSettings.PluginState
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import com.wahyou.app.databinding.ActivityGameBinding
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerFullScreenListener
+import com.wahyou.app.databinding.ActivityVideoBinding
 
 
 class VideoActivity : AppCompatActivity() {
 
-    private val binding : ActivityGameBinding by lazy {
-        ActivityGameBinding.inflate(layoutInflater)
+    private val binding : ActivityVideoBinding by lazy {
+        ActivityVideoBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        // deeplink
-        val action: String? = intent?.action
-        val data: Uri? = intent?.data
-
-        Log.i("Action Video", action.toString());
-        Log.i("Data Video", data.toString());
-
-        // teu jadi di webview keun
-        // setupWebView()
+        setupVideoPlayer()
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
-    private fun setupWebView(){
-        with(binding){
+    private fun setupVideoPlayer() {
+        with(binding) {
+            ytPlayer.also {
+                lifecycle.addObserver(it)
+                it.initialize(
+                        object : AbstractYouTubePlayerListener() {
+                            override fun onReady(youTubePlayer: YouTubePlayer) {
+                                super.onReady(youTubePlayer)
+                                youTubePlayer.cueVideo("UWnviaEXFsg", 0F)
+                                youTubePlayer.play()
+                            }
+                        }, true
+                )
+                it.addFullScreenListener(object : YouTubePlayerFullScreenListener {
+                    override fun onYouTubePlayerEnterFullScreen() {
+                        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                    }
 
-            webView.getSettings().setJavaScriptEnabled(true)
-            webView.getSettings().setPluginState(PluginState.ON)
-            webView.loadUrl("http://www.youtube.com/embed/zZVEI5XkUYU?autoplay=1&vq=small")
-            webView.setWebChromeClient(WebChromeClient())
+                    override fun onYouTubePlayerExitFullScreen() {
+                        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                    }
+                })
+                it.enableBackgroundPlayback(true)
+            }
         }
     }
 }
